15584525a9e4f6945f311afddc4e04fd24a022a0
klmit <klm@smtw.in>
2024-01-20 22:36:47 +0530

    added 111

--- src/javcw111.java
+++ src/javcw111.java
@@ -1,6 +1,41 @@
+
+/*1.	Write a program to add 8 to the number 2345 and then divide it by 3. Now,
+ the modulus of the quotient is taken with 5 and then multiply the resultant value by 5. Display the final result.
+ */
+/*
 public class javcw111 {
     public static void main(String[] args) {
-
-
+        int n = 2345;
+        int n1 =(((n+8)/3)%5)*5;
+        System.out.println(n1);
     }
 }
+*/
+//2
+/*
+public class javcw111 {
+    public static void main(String[] args) {
+        int n = 2345;
+        n+=8;
+        n/=3;
+        n%=5;
+        n*=5;
+        System.out.println(n);
+    }
+}
+*/
+//3
+/*
+public class javcw111 {
+    public static void main(String[] args) {
+        int i = 45;
+        int k = 23;
+        if (i==k){
+            System.out.println("same!");
+        }else {
+            System.out.println("not same");
+        }
+    }
+}
+ */
+
