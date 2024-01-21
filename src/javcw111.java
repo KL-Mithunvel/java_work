6f3bf25148e327c1b2ebc65d2e1506848cd672d7
klmit <klm@smtw.in>
2024-01-21 09:47:28 +0530

    added 111

--- src/javcw111.java
+++ src/javcw111.java
@@ -39,3 +39,62 @@ public class javcw111 {
 }
  */
 
+//4
+/*
+public class javcw111 {
+    public static void main(String[] args) {
+        int a=55;
+        int b=70;
+        if (a<50){
+            System.out.println("True");
+        }
+        if (a<b){
+            System.out.println("True");
+        }
+    }}
+    */
+//5
+/*
+public class javcw111 {
+    public static void main(String[] args) {
+        int m1=45,m2=78,m3=62;
+        int total = m1+m2+m3;
+        int p1=m1/100,p2=m2/100,p3=m3/100;
+        System.out.println("total"+total);
+        System.out.println("persent 1"+p1);
+        System.out.println("persent 2"+p2);
+        System.out.println("persent 3"+p3);
+    }
+}
+*/
+//6
+/*
+public class javcw111 {
+    public static void main(String[] args) {
+        int n= 123,i;
+        String st= ""+n,l="";
+        for (i=0;i<st.length();i++){
+            l=st.charAt(i)+l;
+        }
+        System.out.println(l);
+    }
+}
+*/
+
+//7
+/*
+public class javcw111 {
+    public static void main(String[] args) {
+        double a = 100.235;
+        int x = (int) a;
+        System.out.println(x);
+    }
+}
+*/
+
+//8
+public class javcw111 {
+    public static void main(String[] args) {
+
+    }
+}
