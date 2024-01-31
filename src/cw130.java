405c93705c958d79039d78095fc3a3f340cdf16b
klmit <klm@smtw.in>
2024-01-31 20:01:34 +0530

    stud mark

--- src/cw130.java
+++ src/cw130.java
@@ -87,47 +87,44 @@ Output : Print Student_RegdNo, Student_Name, Student_Age, Student_Marks1, Studen
 Note : All the marks for the student is +Ve, Otherwise print "Invalid Marks"*/
 
 import java.util.Scanner;
-public class cw130 {
-    public class Stud(String name,int regno,int age,int m1,int m2,int m3){
-
-        int sregno=regno;
-        String sname=name;
-        int mar1=m1;
-        int mar2=m2;
-        int mar3=m3;
-
-        public int avg(){
-            int avg= (mar1+mar2+mar3)/3;
-            return avg;
-        }
 
 
-    }
-
-    public static void main(String[] args) {
-        Scanner myscan=new Scanner(System.in);
-        int regno[]=new int[2];
-        String name[]=new String[2];
-        int m1[]=new int[2];
-        int m2[]=new int[2];
-        int m3[]=new int[2];
-
-        for (int i = 0; i < 2; i++) {
-            regno[i] = myscan.nextInt();
-            name[i] = myscan.nextLine();
-            m1[i]= myscan.nextInt();
-            m2[i]= myscan.nextInt();
-            m3[i]=myscan.nextInt();
-        }
-        if (m1[0]>0 && m2[0]>0&& m3[0]>0) {
-            Stud st1 = new Stud(name[0], regno[0], m1[0], m2[0], m3[0]);
-        }else {System.out.println("Invalid marks");}
-
-        }
+public class cw130{
+public static void main(String[] args) {
+Scanner myscan=new Scanner(System.in);
+int regno[]=new int[2];
+String name[]=new String[2];
+int m1[]=new int[2];
+int m2[]=new int[2];
+int m3[]=new int[2];
+int age[]=new int[2];
+for (int i = 0; i < 2; i++) {
+regno[i] = myscan.nextInt();
+name[i] = myscan.next();
+age[i]=myscan.nextInt();
+m1[i]= myscan.nextInt();
+m2[i]= myscan.nextInt();
+m3[i]=myscan.nextInt();}
+if (m1[0]>0 && m2[0]>0&& m3[0]>0) {
+System.out.println("valid marks");
+Stud st1 = new Stud();
+st1.stud(name[0],regno[0],age[0],m1[0],m2[0],m3[0]);
+int x= st1.avg();
+System.out.println(x);
+}else {System.out.println("Invalid marks");}
+if (m1[1]>0 && m2[1]>0&& m3[1]>0) {
+System.out.println("valid marks");
+Stud st2 = new Stud();
+st2.stud(name[1],regno[1],age[1],m1[1],m2[1],m3[1]);
+int x= st2.avg();
+System.out.println(x);
+}else {System.out.println("Invalid marks");}
 
         }
 
 
-    }
-}
+
+
+ }
+
 
