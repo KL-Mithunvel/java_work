f7f9dc9816836ba31c830d2d4c13b8acad31f22f
kl mithunvel <147433270+KL-Mithunvel@users.noreply.github.com>
2024-03-21 23:24:57 +0530

    TernaryOperators

--- src/Operations/TernaryOperators.java
+++ src/Operations/TernaryOperators.java
@@ -1,4 +1,14 @@
 package Operations;
+abstract class TernaryOperator{
+    boolean if_con(int a,int b){
+        return (a>b) ? (true):(false);
+    }
+}
 
-public class TernaryOperators {
+public class TernaryOperators extends TernaryOperator{
+    public void disp(int a,int b){
+        if (if_con(a,b)){
+        System.out.println("a is bigger i think");}
+        else {System.out.println("b is bigger i think");}
+    }
 }
