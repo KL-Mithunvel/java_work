5b00975a036b5c92f46582ce24226bd5dbcc0c47
kl mithunvel <147433270+KL-Mithunvel@users.noreply.github.com>
2024-03-22 19:34:21 +0530

    Operators are done

--- src/Operations/UnaryOperators.java
+++ src/Operations/UnaryOperators.java
@@ -1,4 +1,27 @@
 package Operations;
+interface var_UnaryOperato{
+    public void pls1(int a);
+    public void les1(int a);
+    public int pos_a(int a);
+    public int neg_a(int a);
+    public boolean notof(int a,int b);
+}
+public class UnaryOperators implements var_UnaryOperato{
+    public void pls1(int a){
+        a++;
+    }
+    public void les1(int a){
+        a--;
+    }
+    public int pos_a(int a){
+        return +a;
 
-public class UnaryOperators {
+    }
+    public int neg_a(int a){
+        return -a;
+
+    }
+    public boolean notof(int a,int b){
+        return !(a>b);
+    }
 }
